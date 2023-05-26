c75b3b444f920a0caee8625cc4ab9816f12f1a09
Valle <Valle@DESKTOP-KKHLUPV>
2023-05-25 23:18:04 -0300

    edicion de Sucursal.java y ListaSucursales.java

--- tp2_grupo03/src/main/java/ar/edu/unju/fi/listas/ListaSucursales.java
+++ tp2_grupo03/src/main/java/ar/edu/unju/fi/listas/ListaSucursales.java
@@ -1,5 +1,27 @@
 package ar.edu.unju.fi.listas;
 
-public class ListaSucursales {
+import java.util.ArrayList;
+import java.util.List;
 
+import ar.edu.unju.fi.model.Sucursal;
+
+public class ListaSucursales {
+	
+    private List<Sucursal> sucursales;
+
+    public ListaSucursales() {
+        sucursales = new ArrayList<>();
+    }
+
+    public void agregarSucursal(Sucursal sucursal) {
+        sucursales.add(sucursal);
+    }
+
+    public void eliminarSucursal(Sucursal sucursal) {
+        sucursales.remove(sucursal);
+    }
+
+    public List<Sucursal> getSucursales() {
+        return sucursales;
+    }
 }
--- tp2_grupo03/src/main/java/ar/edu/unju/fi/model/Sucursal.java
+++ tp2_grupo03/src/main/java/ar/edu/unju/fi/model/Sucursal.java
@@ -1,5 +1,45 @@
 package ar.edu.unju.fi.model;
 
 public class Sucursal {
+    private String nombre;
+    private String codigo;
+    private double precio;
+    private String categoria;
+    private double descuento;
 
+    // Constructor vacío
+    public Sucursal() {
+    }
+
+    // Constructor con todos los atributos
+    public Sucursal(String nombre, String codigo, double precio, String categoria, double descuento) {
+        this.nombre = nombre;
+        this.codigo = codigo;
+        this.categoria = categoria;
+    }
+
+    // Métodos getters y setters
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(String categoria) {
+        this.categoria = categoria;
+    }
 }
